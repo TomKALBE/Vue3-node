5335714975d77b29f25dd7384f99e7433bebee87
Tom <t.kalbe@h3hitema.fr>
2022-06-20 14:00:08 +0200

    fixe port of the backend to 3001

--- api/server.js
+++ api/server.js
@@ -33,6 +33,6 @@ app.use(function (err, req, res, next) {
     else res.status(500).json({ message: "Something looks wrong :( !!!" });
 });
 
-app.listen(3000, function () {
-    console.log("Node server listening on port 3000");
+app.listen(3001, function () {
+    console.log("Node server listening on port 3001");
 });
